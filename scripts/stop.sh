e875deff40c2d53f980ee1ba9cd1f426c5c32040
Dennis G <dennis.newyork@gmail.com>
2024-04-10 15:55:56 +0800

    Add stop.sh script

---
+++ scripts/stop.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+max_attempts=300
+command_path=/opt/minecraft/utilities/command.sh
+
+[ -z "$1" ] && exit 1
+
+"$command_path" stop
+
+c=0
+while [ "$(ps -p "$1" -o comm=)" = "bedrock_server" ]
+do
+    sleep 1
+    c=$(expr $c + 1)
+    [ "$c" -ge "$max_attempts" ] && exit 2
+done
+
+exit 0
