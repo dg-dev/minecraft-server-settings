b013d32bfa6f91a1c57d7be5332f619d33d2333a
Dennis G <dennis.newyork@gmail.com>
2024-04-21 17:36:54 +0800

    Update backup.sh to use case and to add debug printing

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,57 +1,131 @@
 #!/bin/sh
 
+
+backups_dir=/opt/minecraft/backup
 command_path=/opt/minecraft/scripts/command.sh
 time_limit_sec=60
+debug=2
+
+debug_print() {
+    debug_level="${1:?debug_level}"
+    shift
+    [ "$debug_level" -le "${debug:-0}" ] && echo "$*"
+}
 
 backup_state=0
+debug_print 3 '$backup_state: '"$backup_state"
 start_epoch=$(date +%s)
 start_cursor=$(journalctl -u minecraft.service --show-cursor -n 0 | grep '^-- cursor: ' | cut -f 3 -d ' ')
+debug_print 4 '$start_cursor: '"$start_cursor"
 file_list=""
+debug_print 3 '$file_list:'"$file_list"
+
 
 while [ "$(expr "$(date +%s)" - "$start_epoch")" -lt "$time_limit_sec" ]; do
     [ -z "$start_cursor" ] && exit 1
 
     if [ "$backup_state" -eq 0 ]; then
+        debug_print 2 ">>> save resume"
         "${command_path}" save resume
         sleep 1
+        debug_print 2 ">>> save hold"
         "${command_path}" save hold
         backup_state=1
+        debug_print 3 '$backup_state: '"$backup_state"
     fi
 
     while read -r LINE; do
-        if [ "$(echo "$LINE" | cut -f '1-2' -d ' ')" = '-- cursor:' ]; then
-            start_cursor="$(echo "$LINE" | cut -f '3' -d ' ')"
-        elif [ "$backup_state" -eq 1 ] && [ ! -z "$(echo "$LINE" | grep 'Saving\.\.\.$')" ]; then
-            echo "LINE (${backup_state}): $LINE"
-            "${command_path}" save query
-            backup_state=2
-        elif [ "$backup_state" -eq 2 ] && [ ! -z "$(echo "$LINE" | grep 'Data saved\. Files are now ready to be copied\.$')" ]; then
-            echo "LINE (${backup_state}): $LINE"
-            "${command_path}" save query
-            backup_state=3
-        elif [ "$backup_state" -eq 2 ] && [ ! -z "$(echo "$LINE" | grep 'A previous save has not been completed\.$')" ]; then
-            echo "LINE (${backup_state}): $LINE"
-            "${command_path}" save query
-        elif [ "$backup_state" -eq 3 ] && [ ! -z "$(echo "$LINE" | grep 'Data saved\. Files are now ready to be copied\.$')" ]; then
-            echo "LINE (${backup_state}): $LINE"
-            echo "LIST: ${file_list}"
-            # backup the files on the list here
-            "$command_path" save resume
-            backup_state=4
-        elif [ "$backup_state" -eq 3 ] && [ ! -z "$(echo "$LINE" | grep -Ev '^\[[0-9-]+ [0-9:]+ [A-Z]+\] ')" ]; then
-            echo "LINE (${backup_state}): $LINE"
-            file_list="${file_list}$LINE"
-        elif [ "$backup_state" -eq 4 ] && [ ! -z "$(echo "$LINE" | grep 'Changes to the world are resumed\.$')" ]; then
-            echo "LINE (${backup_state}): $LINE"
-            exit 0
-        else
-            echo "LINE (${backup_state}): $LINE"
-        fi
+        case "$LINE" in
+            '-- cursor: '* )
+                debug_print 4 "LINE (${backup_state}): $LINE"
+                start_cursor="$(echo "$LINE" | cut -f '3' -d ' ')"
+                debug_print 4 '$start_cursor: '"$start_cursor"
+                ;;
+            *'Saving...' )
+                case "$backup_state" in
+                    1 )
+                        debug_print 1 "LINE (${backup_state}): $LINE"
+                        debug_print 2 ">>> save query"
+                        "${command_path}" save query
+                        backup_state=2
+                        debug_print 3 '$backup_state: '"$backup_state"
+                        ;;
+                    * )
+                        debug_print 1 "<LINE> (${backup_state}): $LINE"
+                        ;;
+                esac
+                ;;
+            *'Data saved. Files are now ready to be copied.' )
+                case "$backup_state" in
+                    2 )
+                        debug_print 1 "LINE (${backup_state}): $LINE"
+                        debug_print 2 ">>> save query"
+                        "${command_path}" save query
+                        backup_state=3
+                        debug_print 3 '$backup_state: '"$backup_state"
+                        ;;
+                    3 )
+                        debug_print 1 "LINE (${backup_state}): $LINE"
+                        debug_print 3 '$file_list: '"$file_list"
+                        # backup the files on the list here
+                        debug_print 2 ">>> save resume"
+                        "$command_path" save resume
+                        backup_state=4
+                        debug_print 3 '$backup_state: '"$backup_state"
+                        ;;
+                    * )
+                        debug_print 1 "<LINE> (${backup_state}): $LINE"
+                        ;;
+                    esac
+                ;;
+            *'A previous save has not been completed.' )
+                case "$backup_state" in
+                    2 )
+                        debug_print 1 "LINE (${backup_state}): $LINE"
+                        debug_print 2 ">>> save query"
+                        "${command_path}" save query
+                        ;;
+                    * )
+                        debug_print 1 "<LINE> (${backup_state}): $LINE"
+                        ;;
+                    esac
+                ;;
+            [![]* )
+                case "$backup_state" in
+                    3 )
+                        debug_print 1 "LINE (${backup_state}): $LINE"
+                        file_list="${file_list}$LINE"
+                        debug_print 3 '$file_list: '"$file_list"
+                        ;;
+                    * )
+                        debug_print 1 "<LINE> (${backup_state}): $LINE"
+                        ;;
+                    esac
+                ;;
+            *'Changes to the world are resumed.' )
+                case "$backup_state" in
+                    4 )
+                        debug_print 1 "LINE (${backup_state}): $LINE"
+                        exit 0
+                        ;;
+                    * )
+                        debug_print 1 "<LINE> (${backup_state}): $LINE"
+                        ;;
+                    esac
+                ;;
+            * )
+                debug_print 1 "<LINE> (${backup_state}): $LINE"
+                ;;
+        esac
     done << EOT
 $(journalctl -u minecraft.service --after-cursor "$start_cursor" -o cat --no-pager --show-cursor)
 EOT
     sleep 1
 done
 
-[ "$backup_state" -gt 0 ] && "$command_path" save resume
+if [ "$backup_state" -gt 0 ]; then
+    debug_print 2 ">>> save resume"
+    "$command_path" save resume
+fi
+
 exit 2
